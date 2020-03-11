d620e53d41cf89460b3c1b3bf64d619f31c71331
Diamond <diamondmiguel12@gmail.com>
2020-03-11 00:49:35 -0400

    Copiando scripts bases

--- ChatterBot/admin.py
+++ ChatterBot/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pregunta, Respuesta, TipoPregunta
+admin.site.register(Pregunta)
+admin.site.register(Respuesta)
+admin.site.register(TipoPregunta)
